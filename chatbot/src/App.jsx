fc62f0a95d3cfb92a101ccdc46b9fc12d39bbfcc
avinashchowdarym <151068758+avinashchowdarym@users.noreply.github.com>
2025-01-28 17:08:16 +0000

    started chatbot integrating deeplink

---
+++ chatbot/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import ChatBot from './components/ChatBot'
+
+function App() {
+
+  return (
+    <div className="App">
+      <ChatBot />
+    </div>
+  )
+}
+
+export default App
